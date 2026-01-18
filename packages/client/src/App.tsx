2d9ad6a14ef37ffdd0640f888718a6b653299a09
Khayitboev Khojiakbar <xayitbaevhojiakbar@gmail.com>
2026-01-18 22:28:39 +0100

    Add Tailwind CSS integration and update styles in the client

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       .then((data) => setMessage(data.message));
   }, []);
 
-  return <p>{message}</p>;
+  return <p className="font-bold">{message}</p>;
 }
 
 export default App;
