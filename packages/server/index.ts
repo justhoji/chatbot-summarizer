4cc59d9cdf4bb3037cc8f41fea9967b1fae48f2d
Khojiakbar Khayitboev <xayitbaevhojiakbar@gmail.com>
2026-01-24 21:28:53 +0100

    Add zod for schema validation in chat endpoint

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, type Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
-
+import z from 'zod';
 dotenv.config();
 
 const client = new OpenAI();
@@ -21,9 +21,26 @@ app.get('/api/hello', (req: Request, res: Response) => {
    });
 });
 
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'The prompt is too short')
+      .max(1000, 'The prompt is too long (max 1000 characters)'),
+
+   conversationId: z.uuid('Invalid UUID'),
+});
+
 const conversations = new Map<string, string>();
 app.post('/api/chat', async (req: Request, res: Response) => {
    const { prompt, conversationId } = req.body;
+   const { success, data, error } = chatSchema.safeParse(req.body);
+   if (!success) {
+      res.status(400).json({
+         error: error,
+      });
+      return;
+   }
    const response = await client.responses.create({
       model: 'gpt-5-nano',
       input: prompt,
