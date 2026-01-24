c2560ff70e3db771c76c2456993519eabae8a975
Khojiakbar Khayitboev <xayitbaevhojiakbar@gmail.com>
2026-01-24 22:26:57 +0100

    Refactor server structure by moving chat endpoint to routes module

---
+++ packages/server/routes.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import { chatController } from './controllers/chat.controller';
+
+const router = express.Router();
+
+router.post('/api/chat', async (req: Request, res: Response) => {
+   chatController.sendMessage(req, res);
+});
+
+export default router;
